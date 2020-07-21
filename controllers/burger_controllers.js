07eb7890a5c4083aba8a5866b607a4d0a2d598c9
Michael Cui <cuimichael500@gmail.com>
2020-07-21 17:18:06 -0500

    created require and export statements

---
+++ controllers/burger_controllers.js
@@ -0,0 +1,23 @@
+// Required depedencies
+var express = require('express');
+var burger = require('../models/burger.js');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Export 
+module.exports = router;
---
+++ models/burger.js
@@ -0,0 +1,11 @@
+// Import the ORM
+var orm = require("../config/orm.js");
+
+
+
+
+
+
+
+// Export 
+module.exports = burger;
