94513c2dd0dcc3a7d78e362d5abfc8e4dbd2e636
Michael Cui <cuimichael500@gmail.com>
2020-07-21 20:33:00 -0500

    added connection.js

--- server.js
+++ server.js
@@ -1,3 +1,22 @@
 // Dependencies
-var express = require("express");
+var express = require('express');
+var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
 
+var PORT = process.env.PORT || 3000;
+
+
+// Handlebars 
+var exphbs = require('express-handlebars');
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+// Import routes
+var routes = require('./controllers/burger_controllers.js');
+app.use('/', routes);
+
+// Initiate listener
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
